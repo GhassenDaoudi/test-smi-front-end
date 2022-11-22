8e4096ef4464712f25a96486e26d3a92996dc302
Ghassen Daoudi <ghassen.daoudi@esprit.tn>
2022-11-22 15:40:33 +0100

    setup root components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Brand from './routes/Domain/Brands/Brand'
-
-
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import './App.css';
+import NavBar from './components/common/NavBar';
+import Brand from './routes/Domain/Brands/Brand';
 
 function App() {
-
   return (
     <BrowserRouter>
-      <Routes   >
-        <Route path="domain">
-          <Route path='brand' >
-            <Route path=":brandId" element={<Brand />} />
-          </Route>
-        </Route>
-      </Routes>
+      <main>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path="domain">
+              <Route path="brand">
+                <Route path=":brandId" element={<Brand />} />
+              </Route>
+            </Route>
+          </Routes>
+        </div>
+      </main>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
